73db2c6d51fbc975412295296360cc5e2936e54c
Pradnyesh Bhadlkar <pradnyeshbhadlkar7247@gmail.comm>
2023-08-03 15:00:11 +0530

    first commit

---
+++ src/Components/page.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import'./page.css';
+function Page() {
+  return (
+    <div className='pagecontainer'>
+      <div className='pagecards'>
+      <div className='Left'>
+      Title of Top services
+        </div>
+      </div>
+      <div className='pagecards'>
+      <div className='Left'>
+      Category of Top services
+        </div>
+      </div>
+      <div className='pagecards'>
+      <div className='Left'>
+      Price of services
+        </div>
+      </div>
+      <div className='pagecards'>
+      <div className='Left'>
+      Discription of service
+        </div>
+      </div>
+      <div className='pagecard'>
+      <div className='Left'>
+      offer on product
+        </div>
+      </div>
+      <div className='pagecar'>
+        <button className='bybutton'>Chose file</button><button className='buybutton'>Upload images of service</button>
+       
+      </div>
+      <div class="pradnyesh">
+
+    <button className='pranav'>Save</button>                                                  <button className='pranav'>Discart</button>
+    
+   
+  
+  </div>
+      
+    </div>
+  )
+}
+
+export default Page
+
